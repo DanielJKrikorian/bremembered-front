d21259867f645fcf1c6279cb220f6b648770b8aa
Daniel Krikorian <Daniel@brememberedproductions.com>
2025-08-15 12:47:59 -0400

    Fix unterminated string in SearchResults

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -293,4 +293,4 @@ export const SearchResults: React.FC = () => {
     return (
       <div className="min-h-screen bg-gray-50 flex items-center justify-center">
         <div className="text-center">
-          <div className="animate-spin w-12 h-12 border
+          <div className="animate-spin w-12 h-12 border-4 border-rose-500 border-t-transparent rounded-full mx-auto mb-4"></div>
