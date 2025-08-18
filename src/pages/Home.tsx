a9e6253c23bb1b6391c1b70e3b6522159638e07d
Daniel Krikorian <Daniel@brememberedproductions.com>
2025-08-18 17:57:26 -0400

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -173,13 +173,12 @@ export const Home: React.FC = () => {
                 <Button
                   variant="primary"
                   size="lg"
-                  onClick={() => {
+                  onClick={(e) => {
                     navigate('/search');
                     // Scroll to top after navigation
                     setTimeout(() => {
                       window.scrollTo({ top: 0, behavior: 'smooth' });
                     }, 100);
-                  }}
                     e.preventDefault();
                     e.stopPropagation();
                     setShowBookingModal(true);
