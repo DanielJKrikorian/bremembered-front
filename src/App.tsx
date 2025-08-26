999e214830ad1c07e110cc75a1cdcb8400f302c1
Daniel Krikorian <Daniel@brememberedproductions.com>
2025-08-26 10:52:20 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from '../context/AuthContext';
-import { CartProvider } from '../context/CartContext';
-import { Checkout } from '../pages/Checkout';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext';
+import { BookingProvider } from './context/BookingContext';
+import { AppContent } from './components/AppContent';
+import { Checkout } from './pages/Checkout'; // Add Checkout import
 
 function App() {
   return (
     <AuthProvider>
       <CartProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/checkout" element={<Checkout />} />
-            {/* Other routes */}
-          </Routes>
-        </BrowserRouter>
+        <BookingProvider>
+          <Router>
+            <Routes>
+              <Route path="/*" element={<AppContent />} />
+              <Route path="/checkout" element={<Checkout />} /> {/* Add Checkout route */}
+            </Routes>
+          </Router>
+        </BookingProvider>
       </CartProvider>
     </AuthProvider>
   );
