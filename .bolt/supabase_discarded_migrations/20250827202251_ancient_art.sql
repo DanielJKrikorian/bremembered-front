a0bcefa1de0b59bb33f03a7e8e5639abb9b63043
Daniel Krikorian <Daniel@brememberedproductions.com>
2025-08-30 17:34:09 -0400

    Deleted 20250826215308_rapid_villa.sql

--- .bolt/supabase_discarded_migrations/20250827202251_ancient_art.sql
+++
@@ -1,6 +0,0 @@
-@@ .. @@
--DROP TRIGGER IF EXISTS send_message_email_webhook_trigger ON messages;
-+DROP TRIGGER IF EXISTS message_email_webhook_trigger ON messages;
-
--- Drop the function that uses pg_net
-DROP FUNCTION IF EXISTS send_message_email_webhook();
