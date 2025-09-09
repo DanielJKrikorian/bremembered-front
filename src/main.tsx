c2e9c992eda3dc2e0ed6b5e494a6f8f8a2dccbb6
DanielJKrikorian <daniel@brememberedproductions.com>
2025-09-09 16:17:08 -0400

    Analytics update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
+// src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import ReactGA from "react-ga4";
+
+// ✅ Initialize GA once here
+ReactGA.initialize("G-JHD0NKW137");
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
