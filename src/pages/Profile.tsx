17e32bea510c4c2d042a83bafc960b04255b4102
DanielJKrikorian <daniel@brememberedproductions.com>
2025-10-07 14:54:19 -0400

    Budget Beta

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -183,7 +183,7 @@ export const Profile: React.FC = () => {
 
   const tabs = [
     { key: 'overview', label: 'Overview', icon: Calendar },
-    { key: 'budget-table', label: 'Budget Table', icon: DollarSign },
+    { key: 'budget-table', label: 'Budget Table BETA', icon: DollarSign },
     { key: 'wedding-website', label: 'Wedding Website', icon: Globe },
     { key: 'notes', label: 'Wedding Notes', icon: StickyNote },
     { key: 'wedding-board', label: 'Wedding Board', icon: Heart },
